de7909c4e6a4b13983c95d2742caf235fababc68
jonny030 <tow0305@gmail.com>
2023-06-12 21:55:17 +0800

    更改Controller格式

---
+++ controllers/testController.js
@@ -0,0 +1,6 @@
+module.exports = class testController{
+    index(req, res) {
+        // res.render('hello');
+        res.send('hello');
+    }
+}
--- routes/web.js
+++ routes/web.js
@@ -2,27 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* 引入 Controller */
-const todoController = require('../controllers/todo');
+const testController = new (require('../controllers/testController'))
 
 /* 設定Web Router. */
 router.get('/', function (req, res, next) {
-  res.render('hello');
+    res.render('hello');
 });
 
+router.get('/index', testController.index);
+
 router.get('/hello', (req, res) => {
-  res.render('hello');
+    res.render('hello');
 });
 
 router.get('/search', (req, res) => {
-  res.render('search');
+    res.render('search');
 });
 
 router.post('/searchList', (req, res) => {
-  res.send(req.body);
+    res.send(req.body);
 });
-
-router.get('/todos', todoController.getAll);
-
-router.get('/test', todoController.getDB);
 
 module.exports = router;
